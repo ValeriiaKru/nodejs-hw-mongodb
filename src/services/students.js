ff46cbc42a01ccbba00dbec436f0a2a0021d7b23
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-10-02 12:27:37 +0300

    message

---
+++ src/services/students.js
@@ -0,0 +1,19 @@
+import { StudentsCollection } from '../db/models/student.js';
+
+export const getAllStudents = async () => {
+  const students = await StudentsCollection.find();
+  return students;
+};
+
+export const getStudentById = async (studentId) => {
+  const student = await StudentsCollection.findById(studentId);
+  return student;
+};
+
+app.get('/students', async (req, res) => {
+
+});
+
+app.get('/students/:studentId', async (req, res) => {
+
+});
