6be0142f680091c9f4465356c991ae43876fa691
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-10-12 10:26:29 +0300

    message

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ export const startServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });
@@ -44,6 +45,7 @@ export const startServer = () => {
 
 
     res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   });
