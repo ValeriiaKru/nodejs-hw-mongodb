bd0ba445fc4bd803a7b52fbd67eda9ea1fb9ea6b
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-10-16 09:52:11 +0300

    message

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { isValidId } from '../middlewares/isValidId.js';
 import {
   getContactsController,
   getContactByIdController,
@@ -8,18 +8,19 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema,updateContactSchema } from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-
-router.post('/contacts', ctrlWrapper(createContactController));
-
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
-
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/contacts', validateBody(contactSchema), ctrlWrapper(createContactController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController)
+);
 export default router;
-
