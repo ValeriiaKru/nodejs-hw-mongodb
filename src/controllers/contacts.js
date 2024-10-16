42a2e204d37d8330b9155b8953b3e3334069b822
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-10-16 11:18:43 +0300

    message

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,107 +1,69 @@
+import {
+  createContact,
+  deleteContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
-import { createContact,deleteContact,updateContact,getAllContacts, getContactById } from '../services/contacts.js';
-import { parsePaginationParams } from '../utils/parsePaginationParams.js';
-import { parseSortParams } from '../utils/parseSortParams.js';
-import { parseFilterParams } from '../utils/parseFilterParams.js';
 
-
-export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
-
+export async function getAllContactsController(req, res) {
+  const allContacts = await getAllContacts();
   res.json({
     status: 200,
     message: 'Successfully found contacts!',
-    data: contacts,
+    data: allContacts,
   });
-};
+}
 
-export const getContactByIdController = async (req, res) => {
+export async function getContactByIdController(req, res, next) {
   const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-  
 
-  if (!contact) {
+  const contact = await getContactById(contactId);
+
+  if (contact === null) {
     throw createHttpError(404, 'Contact not found');
   }
-
   res.json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
-};
+}
 
-// export const createContactController = async (req, res) => {
-//   const contact = await createContact(req.body);
+export async function createContactController(req, res, next) {
+  const result = await createContact(req.body);
 
-//   res.status(201).json({
-//     status: 201,
-//     message: `Successfully created a contact!`,
-//     data: contact,
-//   });
-// };
-
-// export const getStudentsController = async (req, res) => {
-//   const { page, perPage } = parsePaginationParams(req.query);
-//   const students = await getAllStudents({
-//     page,
-//     perPage,
-//   });
-
-//   res.json({
-//     status: 200,
-//     message: 'Successfully found students!',
-//     data: students,
-//   });
-// };
-
-export const getStudentsController = async (req, res) => {
-  const { page, perPage } = parsePaginationParams(req.query);
-  const { sortBy, sortOrder } = parseSortParams(req.query);
-  const filter = parseFilterParams(req.query);
-
-  const students = await getAllStudents({
-    page,
-    perPage,
-    sortBy,
-    sortOrder,
-    filter,
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: result,
   });
+}
 
-  res.json({
-    status: 200,
-    message: 'Successfully found students!',
-    data: students,
-  });
-};
+export async function updateContactController(req, res, next) {
+  const { contactId } = req.params;
 
-export const deleteContactController = async (req, res, next) => {
+  const result = await updateContact(contactId, req.body);
 
-    const { contactId } = req.params;
-
-    const contact = await deleteContact(contactId);
-
-    if (!contact) {
-      throw createHttpError(404, 'Contact not found');
-    }
-
-    res.status(204).send(); 
+  if (result === null) {
+    throw createHttpError(404, 'Contact not found');
   }
 
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result,
+  });
+}
 
+export async function deleteContactController(req, res, next) {
+  const { contactId } = req.params;
+  const result = await deleteContact(contactId);
 
-export const patchContactController = async (req, res, next) => {
-    const { contactId } = req.params;
-    const updatedData = req.body;
+  if (result === null) {
+    throw createHttpError(404, 'Contact not found');
+  }
 
-    const updatedContact = await updateContact(contactId, updatedData);
-
-    if (!updatedContact) {
-      throw createHttpError(404, 'Contact not found');
-    }
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully patched a contact!',
-      data: updatedContact,
-    });
-};
+  res.status(204).send();
+}
