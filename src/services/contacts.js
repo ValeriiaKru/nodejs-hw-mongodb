3bffecb31ce3b964be8f7cc5a4291b1062e8e655
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-10-22 14:08:36 +0300

    hw-4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,42 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export async function getAllContacts() {
-  const contacts = await ContactsCollection.find();
-  return contacts;
-}
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+  if (filter.isFavourite) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const contactsCount = await ContactsCollection.countDocuments(contactsQuery);
+  const total = Math.ceil(contactsCount / perPage);
+  const data = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder });
+  return {
+    data,
+    page,
+    perPage,
+    totalItems: contactsCount,
+    totalPages: total,
+    hasPreviousPage: page > 1,
+    hasNextPage: totalPages - page > 0,
+
+  };
+
+};
 
 export async function getContactById(contactId) {
   const contact = await ContactsCollection.findById(contactId);
