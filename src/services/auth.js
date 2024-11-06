6ae4e76f4adc7dddea6e3a28ea0a550ac0fe8bef
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-11-06 20:58:54 +0200

    message

--- src/services/auth.js
+++ src/services/auth.js
@@ -99,31 +99,51 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
 
 
 export async function resetPassword(payload) {
-  let entries;
-
-  try {
-    entries = jwt.verify(payload.token, env('JWT_SECRET'));
-  } catch (err) {
-    if (err instanceof Error)
-      throw createHttpError(401, 'Token is expired or invalid.');
-    throw err;
-  }
-
-  const user = await UsersCollection.findOne({
-    email: entries.email,
-    _id: entries.sub,
-  });
+    const user = await UsersCollection.findOne({ email });
 
   if (!user) {
-    throw createHttpError(404, 'User not found');
+    throw createHttpError(404, 'User not found!');
   }
 
-  const encryptedPassword = await bcrypt.hash(payload.password, 10);
-
-  await UsersCollection.updateOne(
-    { _id: user._id },
-    { password: encryptedPassword },
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '5m',
+    },
   );
+
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password.html',
+  );
+
+  const templateSourse = (
+    await fs.readFile(resetPasswordTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSourse);
+  const html = template({
+    name: user.name,
+    link: `${env('APP_DOMAIN')}/auth/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: env('SMTP_FROM'),
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch (error) {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
 }
 
 
